482535012f6de3e9534590c2419157916f136ea4
Garrett Takei <takei001@gannon.edu>
2023-11-12 20:14:15 -0500

    Make a package directory for test cases

---
+++ src/tests/DescriptionUnitTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.*;
+
+public class DescriptionUnitTest {
+    // Test creating descriptions
+    @Test
+    public void CreateDescription() {
+    }
+
+    // Test deleting descriptions
+    @Test
+    public void DeleteDescription() {
+
+    }
+
+    // Test updating descriptions
+    @Test
+    public void UpdateDescription() {
+
+    }
+}
